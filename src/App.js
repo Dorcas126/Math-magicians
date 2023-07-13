3693ef4e514fe0296de8ffb3338e1299b8b559f2
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-07-13 23:09:30 +0200

    Display quote on the webpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import './App.css';
 import React from 'react';
 import Calculator from './components/calculator';
+import FetchQuote from './components/ApiData';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
-}
+const App = () => (
+  <div className="container">
+    <header className="title">
+      <h1>Math-magicians</h1>
+    </header>
+    <FetchQuote />
+    <Calculator />
+  </div>
+);
 
 export default App;
